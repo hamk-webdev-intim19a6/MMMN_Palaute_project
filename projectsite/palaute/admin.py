14107a80dd7081aa32033e236713a8257c369921
Marjaana88 <marjaana.manninen@student.hamk.fi>
2020-09-25 07:05:57 +0000

    Created databases Kayttajat, Palaute, Toimipiste

--- projectsite/palaute/admin.py
+++ projectsite/palaute/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Choice, Question, Kayttajat, Toimipiste, Palaute
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -17,3 +17,32 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+
+class ToimipisteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Toimipisteen nimi',               {'fields': ['toimipiste_nimi']}),
+        ('Pvm tiedot', {'fields': ['last_update'], 'classes': ['collapse']}),
+        ('Osoitteen tiedot', {'fields': ['osoite_id']})
+    ]
+    list_display = ('toimipiste_nimi', 'last_update')
+    list_filter = ['last_update']
+    search_fields = ['toimipiste_nimi']
+
+admin.site.register(Toimipiste, ToimipisteAdmin)
+
+
+class PalauteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Toimipiste, jota palaute koskee', {'fields': ['toimipiste_id']}),
+        (None, {'fields': ['palaute_pvm'], 'classes': ['collapse']}),
+        (None, {'fields': ['arvosana']}),
+        ('Hyvää', {'fields': ['hyvaa']}),
+        (None, {'fields': ['huonoa']}),
+        (None, {'fields': ['parannettavaa']}),
+    ]
+    list_display = ('toimipiste_id', 'palaute_pvm')
+    list_filter = ['toimipiste_id']
+    search_fields = ['toimipiste_id']
+
+admin.site.register(Palaute, PalauteAdmin)
+    
--- projectsite/palaute/models.py
+++ projectsite/palaute/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# Testi models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,6 +12,60 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
+
+
+# Palauteprojekti models
+# Django luo id pääavaimen tauluille automaattisesti
+
+class Kayttajat(models.Model):
+    '''Many-to-one relationship'''
+    osoite_id = models.ForeignKey(
+        'Osoite',
+        on_delete=models.RESTRICT
+    )
+    etunimi = models.CharField(max_length=30)
+    sukunimi = models.CharField(max_length=30)
+    kayttaja_nimi = models.CharField(max_length=30)
+    salasana = models.CharField(max_length=30)
+    sahkoposti = models.CharField(max_length=30)
+    '''Automatically set the field to now every time the object is saved. 
+    Useful for “last-modified” timestamps '''
+    last_update = models.DateField(auto_now=False)
+
+
+class Toimipiste(models.Model):
+    last_update = models.DateField(auto_now=False)
+    '''One-to-one relationship'''
+    osoite_id = models.OneToOneField(
+        'Osoite',
+        on_delete=models.RESTRICT
+    )
+    toimipiste_nimi = models.CharField(max_length=30)
+
+
+class Palaute(models.Model):
+    '''Automatically set the field to now when the object is first created. 
+    Useful for creation of timestamps'''
+    palaute_pvm = models.DateField(auto_now_add=False)
+    '''Many-to-one relationship'''
+    kayttaja_id = models.ForeignKey(
+        Kayttajat,
+        on_delete=models.RESTRICT
+    )
+    '''Many-to-one relationship'''
+    toimipiste_id = models.ForeignKey(
+        Toimipiste,
+        on_delete=models.RESTRICT
+    )
+    arvosana = models.PositiveIntegerField()
+    hyvaa = models.TextField()
+    huonoa = models.TextField()
+    parannettavaa = models.TextField()
+
+
+class Osoite(models.Model):
+    pass
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)

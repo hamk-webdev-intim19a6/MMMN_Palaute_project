b15f1d892d92b91fad18a39a5d9e5dd183dff07d
Marjaana88 <marjaana.manninen@student.hamk.fi>
2020-10-03 12:43:57 +0000

    Created user registration and login

--- projectsite/palaute/urls.py
+++ projectsite/palaute/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /palaute/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('index/', views.index, name="index"),
 ]
--- projectsite/palaute/views.py
+++ projectsite/palaute/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, Http404
 from django.template import loader
-from .models import Question
+from .models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
+from django.contrib import messages
+
+from .forms import CreateUserForm
 # Create your views here.
 
 
@@ -21,3 +26,38 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're giving feedback on topic %s." % question_id)
+
+def register_page(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Tili on luotu käyttäjälle' + user)
+            #return redirect('login')
+            #return redirect('/palaute/login/')
+
+    context = { 'form':form}
+    return render(request, 'palaute/register.html', context)
+
+
+def login_page(request):
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            #return redirect('index')
+            return redirect ('/palaute/index/')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {'form':form}
+    return render(request, 'palaute/login.html', context)

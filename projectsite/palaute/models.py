611091a23647d61df2ce82f21f2b44fd221f6267
Marjaana88 <marjaana.manninen@student.hamk.fi>
2020-10-11 08:27:07 +0000

    Updated database models and settings

--- projectsite/palaute/models.py
+++ projectsite/palaute/models.py
@@ -18,40 +18,26 @@ class Question(models.Model):
 # Palauteprojekti models
 # Django luo id pääavaimen tauluille automaattisesti
 
-
-class Kaupunki(models.Model):
-    kaupunki_id = models.AutoField(primary_key=True)
-    last_update = models.DateField(auto_now=False)
-    kaupunki = models.CharField(max_length=30)
-    def __str__(self):
-        return self.kaupunki
-
 class Osoite(models.Model):
-    osoite_id = models.AutoField(primary_key=True)
     last_update = models.DateField(auto_now=False)
     kaupunki = models.CharField(max_length=30)
     osoite = models.CharField(max_length=30)
     postinumero = models.CharField(max_length=30)
+    def __str__(self):
+        return "%s, %s" % (self.osoite, self.kaupunki)
 
-    FK_kaupunki_id = models.ForeignKey(
-        'Kaupunki',
-        on_delete=models.RESTRICT
-    )
 
 class Toimipiste(models.Model):
-    toimipiste_id = models.AutoField(primary_key=True)
     last_update = models.DateField(auto_now=False)
-    '''One-to-one relationship'''
     FK_osoite_id = models.OneToOneField(
         'Osoite',
         on_delete=models.RESTRICT
     )
     toimipiste_nimi = models.CharField(max_length=30)
-
+    def __str__(self):
+        return "%s, %s" % (self.toimipiste_nimi, self.FK_osoite_id.kaupunki)
 
 class Kayttajat(models.Model):
-    '''Many-to-one relationship'''
-    kayttaja_id = models.AutoField(primary_key=True)
     FK_osoite_id = models.ForeignKey(
         'Osoite',
         on_delete=models.RESTRICT
@@ -61,21 +47,10 @@ class Kayttajat(models.Model):
     kayttaja_nimi = models.CharField(max_length=30)
     salasana = models.CharField(max_length=30)
     sahkoposti = models.CharField(max_length=30)
-    '''Automatically set the field to now every time the object is saved. 
-    Useful for “last-modified” timestamps '''
     last_update = models.DateField(auto_now=False)
-
-
+    
 class Palaute(models.Model):
-    palaute_id = models.AutoField(primary_key=True)
-    '''Automatically set the field to now when the object is first created. 
-    Useful for creation of timestamps'''
     palaute_pvm = models.DateField(auto_now=False)
-    '''Many-to-one relationship'''
-    FK_kayttaja_id = models.ForeignKey(
-        'Kayttajat',
-        on_delete=models.RESTRICT
-    )
     '''Many-to-one relationship'''
     FK_toimipiste_id = models.ForeignKey(
         'Toimipiste',
@@ -85,6 +60,8 @@ class Palaute(models.Model):
     hyvaa = models.TextField()
     huonoa = models.TextField()
     parannettavaa = models.TextField()
+    def __str__(self):
+        return self.FK_toimipiste_id.toimipiste_nimi
 
 
 class Choice(models.Model):

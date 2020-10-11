551e1f839f681c7f0325e1de5bf58bc4138f0da6
Marjaana88 <marjaana.manninen@student.hamk.fi>
2020-10-11 14:48:40 +0000

    git repo updated

--- projectsite/palaute/views.py
+++ projectsite/palaute/views.py
@@ -12,14 +12,14 @@ from .forms import CreateUserForm, PalauteForm
 
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'palaute/index.html', context)
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'palaute/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'palaute/detail.html', {'question': question})
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'palaute/detail.html', {'question': question})
 
 @login_required
 def results(request, question_id):
